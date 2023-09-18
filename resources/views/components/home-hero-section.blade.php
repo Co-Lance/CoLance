14af242d1bbd50291cc8c843b93bc91654932fe5
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-09-19 00:37:29 +0100

    changes made to banner section

--- resources/views/components/home-hero-section.blade.php
+++ resources/views/components/home-hero-section.blade.php
@@ -6,8 +6,8 @@
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </a>
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-800" >Flexibility meets opportunity in Co-lance.</h1>
-            <p class="max-w-2xl mb-6 font-md text-gray-600 lg:mb-8 md:text-lg lg:text-xl" >Co-Lance, delivering with style and speed,
-                From door to door, we cater to your need.</p>
+            <p class="max-w-2xl mb-6 font-md text-gray-600 lg:mb-8 md:text-lg lg:text-xl" >"Co-Lance, delivering with expertise and efficiency,
+Bringing your projects to life, with precision and proficiency."</p>
             <a href="{{ url('/auth') }}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-teal-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300" >
                 Get started
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
