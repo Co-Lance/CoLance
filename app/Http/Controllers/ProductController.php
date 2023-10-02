04e97a78f004253145a83738a41dfd03d42d2f7e
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-10-02 23:42:56 +0100

    crud for product entity done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -11,5 +13,56 @@ class ProductController extends Controller
         $listproducts = \App\Models\Product::all();
         return view('product.index',compact('listproducts'));
     }
-    
+    public function addProduct()
+    {
+        return view('product.addProduct');
+    }
+    public function storeProduct(ProductRequest $request)
+    {
+        // Validation passed, create and store the product
+        Product::create($request->validated());
+        return redirect()->route('products.index')->with('success', 'Product added successfully!');
+    }
+
+    public function delete($id)
+{
+    $product = Product::find($id);
+
+    if (!$product) {
+        return response()->json(['message' => 'Product not found'], 404);
+    }
+
+    $product->delete();
+
+    // Redirect back or to a desired page after deletion
+    return redirect()->back()->with('success', 'Product deleted successfully');
+}
+    public function edit($id)
+{
+    $product = Product::find($id);
+
+    if (!$product) {
+        return response()->json(['message' => 'Product not found'], 404);
+    }
+
+    // You can load a view to edit the product and pass the product data to the view
+    return view('product.edit')->with('product', $product);
+}
+public function update(Request $request, $id)
+{
+    // Find the product by its ID
+    $product = Product::findOrFail($id);
+
+    // Update the product with the new data
+    $product->update([
+        'name' => $request->input('name'),
+        'image' => $request->input('image'),
+        'user_full_name' => $request->input('user_full_name'),
+        'quantity' => $request->input('quantity'),
+        'description' => $request->input('description'),
+    ]);
+
+    // Redirect to the product details page or any other appropriate route
+    return redirect()->route('products.index')->with('success', 'Product added successfully!');
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,5 +20,13 @@ Route::get('/', function () {
 });
 
 Route::get('/auth', [AuthController::class, 'index']);
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/addProduct', [ProductController::class, 'addProduct']);
+Route::post('/storeProduct', [ProductController::class, 'storeProduct'])->name('products.store');
+Route::get('/products/delete/{id}', [ProductController::class, 'delete'])->name('products.delete');
+Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/edit/mod/{id}', [ProductController::class, 'update'])->name('products.update');
+
+
+
 
