b61573aca190b34dec6e401e5e2d779eb06c23e8
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-10-03 12:19:45 +0100

    merged

--- resources/views/offre/index.blade.php
+++ resources/views/offre/index.blade.php
@@ -35,11 +35,13 @@
             </div>
             <div class="pr-16 mt-10">
                 <div class="p-4">
-                    <div id="menu" class="flex flex-col space-y-2">
-                        <a href="{{route('offres')}}" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">My Offers</a>
+                <div id="menu" class="flex flex-col space-y-2">
+                    <a href="{{ url('/products') }}" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">Products</a>
+                        <a href="{{ url('/addProduct') }}" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">Add Product</a>
+                        <a class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out" href="{{url('offres')}}">My offers</a>
                         <a href="{{route('createoffre')}}" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out" >Create Offer</a>
-                        <a href="#" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">Off</a>
-                        <a href="#" class="text-sm text-white font-medium py-2 px-2 hover:bg-red-700 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">Settings</a>
+
+ 
                     </div>
                 </div>
             </div>
